c945ab7ed2834cedfb426d0bfab3923e7146f5ee
Svetlana210 <svetik.may24@gmail.com>
2022-12-07 20:32:18 +0100

    task

--- src/index.js
+++ src/index.js
@@ -1,81 +1,117 @@
-// import './css/styles.css';
-// import debounce from 'lodash.debounce';
-// import Notiflix from 'notiflix';
-// import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// const DEBOUNCE_DELAY = 300;
+const refs = {
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadBtn: document.querySelector('.load-more'),
+};
 
-// const refs = {
-//   input: document.querySelector('#search-box'),
-//   countryList: document.querySelector('.country-list'),
-//   countryDiv: document.querySelector('.country-info'),
-// };
+const createGalleryTemplate = hits => {
+  return `
+  <a class="gallery-link" href="${hits.largeImageURL}"><img src="${hits.webformatURL}" alt="${hits.tags}" loading="lazy" width="300px" height="180px"/>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b> ${hits.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b> ${hits.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b> ${hits.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b> ${hits.downloads}
+    </p>
+  </div> </a>
+`;
+};
 
-// const clearCountryData = () => {
-//   refs.countryList.innerHTML = '';
-//   refs.countryDiv.innerHTML = '';
-// };
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: false,
+});
 
-// const getArrayTemplate = ({ name, flags }) => {
-//   return `<li class="item"><img src="${flags.svg}" alt="${name.official}" width="70" height="40">${name.official}</li>`;
-// };
-// const getCountryTemplate = ({
-//   name,
-//   capital,
-//   population,
-//   flags,
-//   languages,
-// }) => {
-//   return `<h1 class="country-heading">
-//       <img class="flag" src="${flags.svg}" alt="..." width=80" height="50" />
-//       ${name.official}
-//     </h1>
-//     <p class="info">Capital: <span class="capital">${capital}</span></p>
-//     <p class="info">Population: <span class="popualtion">${population}</span></p>
-//     <p class="info">Languages: <span class="language">${Object.values(
-//       languages
-//     )}</span></p>`;
-// };
-// let items = [];
-// const renderArray = () => {
-//   const list = items.map(getArrayTemplate).join('');
+let text = '';
+let page = 1;
+let images = [];
+let per_page = 40;
+// let total = 0;
+refs.loadBtn.classList.remove('show');
 
-//   refs.countryList.innerHTML = '';
-//   refs.countryList.insertAdjacentHTML('beforeend', list);
-// };
-// const renderCountry = () => {
-//   // console.log(items);
-//   const country = items.map(getCountryTemplate).join('');
+const render = () => {
+  const list = images.map(createGalleryTemplate).join('');
 
-//   refs.countryDiv.innerHTML = '';
-//   refs.countryDiv.insertAdjacentHTML('beforeend', country);
-// };
+  refs.gallery.insertAdjacentHTML('beforeend', list);
+};
 
-// const searchCountry = e => {
-//   e.preventDefault();
-//   const value = e.target.value.trim();
-//   clearCountryData();
-//   if (value !== '') {
-//     fetchCountries(value)
-//       .then(countries => {
-//         if (countries.length > 10) {
-//           Notiflix.Notify.info(
-//             'Too many matches found. Please enter a more specific name.'
-//           );
-//         } else if (countries.length > 1 && countries.length <= 10) {
-//           items = countries;
-//           // clearCountryData();
-//           renderArray();
-//         } else if (countries.length === 1) {
-//           items = countries;
-//           // clearCountryData();
-//           renderCountry();
-//         }
-//       })
-//       .catch(error => {
-//         Notiflix.Notify.failure('Oops, there is no country with that name');
-//       });
-//   }
-// };
+const fetchImages = text => {
+  return fetch(
+    `https://pixabay.com/api/?key=31871227-3dd1acdf50445da59bff19bb0&q=${text}&u=image_type=photo&orientation=horizontal&saferesearch=true&page=${page}&per_page=${per_page}`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(photos => {
+      // total = photos.totalHits;
+      // console.log(total);
+      page += 1;
+      const totalPage = photos.totalHits / per_page;
+      if (photos.hits.length === 0) {
+        refs.loadBtn.classList.remove('show');
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+      if (totalPage > page) {
+        Notiflix.Notify.success(`Hooray! We found ${photos.totalHits} images.`);
+        refs.loadBtn.classList.add('show');
 
-// refs.input.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+        return photos.hits;
+      } else {
+        refs.loadBtn.classList.remove('show');
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    });
+};
+
+const onSubmit = e => {
+  e.preventDefault();
+  refs.gallery.innerHTML = '';
+  text = e.target.elements.searchQuery.value.trim();
+  if (text != '') {
+    page = 1;
+    // console.log(value);
+    fetchImages(text).then(hits => {
+      images = hits;
+      render();
+    });
+  } else {
+    Notiflix.Notify.failure('Write something');
+    return;
+  }
+};
+const onLoadMore = () => {
+  fetchImages(text).then(
+    hits => {
+      // if () {
+      //   alert(`We're sorry, but you've reached the end of search results.`);
+      // } else {
+      images = hits;
+      render();
+    }
+
+    // }
+  );
+};
+
+refs.form.addEventListener('submit', onSubmit);
+refs.loadBtn.addEventListener('click', onLoadMore);
